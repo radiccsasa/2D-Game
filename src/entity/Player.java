9dc4bc67a053ef286ca73421d471286e0e4b4212
Sale2 <radicsasa.gimkm@gmail.com>
2025-08-26 13:15:59 +0200

    Added objects collision

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,7 +14,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
-
+    int hasKey = 0;
     public Player(GamePanel gp, KeyHandler keyH)
     {
         this.gp = gp;
@@ -26,6 +26,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -76,9 +78,14 @@ public class Player extends Entity{
                 direction = "right";
             }
 
-            //proveri collision
+            //proveri tile collision
             collisionOn = false;
             gp.cChecker.checkTile(this);
+
+            //check object collision
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
 
             if (collisionOn == false)
             {
@@ -113,6 +120,28 @@ public class Player extends Entity{
                     spriteNum = 1;
                 }
                 spriteCounter = 0;
+            }
+        }
+    }
+
+    public void pickUpObject(int i){
+        if (i != 999){
+            String objectName = gp.obj[i].name;
+
+            switch (objectName){
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: " +hasKey);
+                    break;
+                case "Door":
+                    if (hasKey > 0)
+                    {
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: " +hasKey);
+                    break;
             }
         }
     }
